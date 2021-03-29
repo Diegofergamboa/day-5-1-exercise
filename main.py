356b49ddb27736f71f45898eb1f0cb4aec8fc97f
Diegofergamboa <diegofergamboa@hotmail.es>
2021-03-29 18:46:37 +0000

    Done

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ student_heights = input("Input a list of student heights ").split(" ")
 for n in range(0, len(student_heights)):
 	student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+
 totalheight = 0
 for height in student_heights :
 	totalheight =+ height
@@ -11,9 +15,5 @@ number_students = 0
 for student in student_heights :
 	number_students =+ 1
 print(totalheight / number_students)
-
-#Write your code below this row 👇
-
-
 
 
